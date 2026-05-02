195a79caf80f026a53c74ae49b0b790d81113c99
stephanhumannz <stephanhumannz@gmail.com>
2026-05-02 19:21:03 +1200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div style={{ backgroundColor: 'black', color: 'red', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', fontFamily: 'monospace' }}>
+      <h1>DIVINE ELITE BRIDGE ONLINE</h1>
+    </div>
+  )
+}
